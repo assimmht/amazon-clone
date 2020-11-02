54789477e9773beec0a9252fff2a0c86a88157cd
assimmht <assimmht235@gmail.com>
2020-11-02 18:30:42 +0100

    Updates

---
+++ precache-manifest.b5a5e25d569e1a766828fab6a1d7734e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5646943e8e3ff8353f025a59d3c3e988",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "41ae7cb7b402cf09fa1b",
+    "url": "/amazon-clone/static/css/main.d44d0f24.chunk.css"
+  },
+  {
+    "revision": "6a8d2cedb531d00c56d3",
+    "url": "/amazon-clone/static/js/2.c6d25a19.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/amazon-clone/static/js/2.c6d25a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ae7cb7b402cf09fa1b",
+    "url": "/amazon-clone/static/js/main.5763a15a.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
